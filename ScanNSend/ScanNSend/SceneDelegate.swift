4d51cfa46e86bfdd444e47c6724c607caa83f59f
Ishan Kansara <IshanK@lightwavetechnology.com>
2020-12-18 16:04:11 -0500

    Made changes to update bundle identifier and added custom url scheme

--- ScanNSend/ScanNSend/SceneDelegate.swift
+++ ScanNSend/ScanNSend/SceneDelegate.swift
@@ -47,6 +47,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
     }
 
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+            if let url = URLContexts.first?.url {
+                print(url)
+                let urlStr = url.absoluteString //1
+                // Parse the custom URL as per your requirement.
 
+
+                }
+            }
 }
 
